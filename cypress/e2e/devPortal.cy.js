c9484bec1d1b54005babda53b711e5548d5f16d5
Imin <imin.isakov@vizio.com>
2023-03-30 10:46:20 -0700

    point env to prod

--- cypress/e2e/devPortal.cy.js
+++ cypress/e2e/devPortal.cy.js
@@ -4,5 +4,5 @@ beforeEach(() => {
 
 it('greets the user with a welcome message', () => {
   cy.url()
-    .should('equal', 'https://preprod.developer.vizio.com/login');
+    .should('equal', 'https://developer.vizio.com/login');
 });
